40ecc2fa921539b52486f4964ba65a5cfbe33bb3
Maxigong <maxi@nolgong.com>
2022-02-01 01:56:22 +0900

    edits

--- vegetarian_blog/settings.py
+++ vegetarian_blog/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y@@vk&(e-qoft-zhg(^@0-q)r#@miv0d87w^7#8^f@7m2^#5*r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['vegetarian-blog.herokuapp.com']
+ALLOWED_HOSTS = ['vegetarian-blog.herokuapp.com', '127.0.0.1:8000/']
 
 
 # Application definition
